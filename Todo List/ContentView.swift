0433f78832db3679b42d14ff5243e21f9fe9c5d1
Bùi Ly Nguyễn <nguyenbuily.it@gmail.com>
2020-10-13 22:10:53 +0700

    Initial Commit

---
+++ Todo List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo List
+//
+//  Created by Nguyen Bui Ly on 10/13/20.
+//  Copyright © 2020 Nguyen Bui Ly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
